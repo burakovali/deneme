40a0de8ea4472a18b622f67a4dadebc45ea7d0df
Burak Ovali <burak.ovali@airties.com>
2023-04-06 12:44:07 +0300

    d

---
+++ rs_pull.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#if [ -z "$1" ]; then
+#	echo "Please provide domain user name like burak.ovali"
+#	exit
+#fi
+
+#git remote add rs-di_remote ssh://$1@git.corp.airties.com:29418/rs-di
+
+#git subtree add --prefix=rs-di/ rs-di_remote master
+
+git subtree pull --prefix=rs-di rs-di_remote master  
+
